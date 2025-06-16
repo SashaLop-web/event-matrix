387ca59e496a93bb31c1f9ed249fc4e58550d894
SashaLop-web <sashahalp05@gmail.com>
2025-06-16 03:38:57 +0300

    nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,7 @@ import 'dotenv/config'
 export default defineNuxtConfig({
 	css: ['@/assets/styles/main.scss'],
 
-	vite: {
-		ssr: {
-			noExternal: ['sqlite3'],
-		},
-	},
-
+	
 	runtimeConfig: {
 		jwtSecret: process.env.JWT_SECRET || 'default_fallback_key',
 		public: {
@@ -20,7 +15,6 @@ export default defineNuxtConfig({
 	modules: ['@pinia/nuxt'],
 	plugins: ['~/plugins/auth.ts'],
 
-	// 👇 Добавить это
 	server: {
 		port: process.env.PORT || 3000,
 		host: '0.0.0.0',
@@ -29,8 +23,6 @@ export default defineNuxtConfig({
 	nitro: {
 		preset: 'node',
 	},
-
-	compatibilityDate: '2024-11-01',
 
 	devtools: {
 		enabled: true,
